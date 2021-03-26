74a5096dc2cf57011d7b166614bb567303ab859d
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2021-03-26 15:28:42 +0700

    menambahkan sedikit tentang var

---
+++ pemrograman-rust/src/02-01/variabel-mutable/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let mut var1 = 0;                   // <1>
+    println!("var1 berisi: {}", var1);
+    var1 = 1;                           // <2>
+    println!("var1 berisiL {}", var1);
+
+}
